b722a99349068ef90b4485c07cca9eb26c6679dd
CalewuLands <92369055+CalewuLands@users.noreply.github.com>
2021-10-13 17:49:21 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+import calendar
+# Write code by Calewuga
+# Original Code from: Clever Programmer (Youtube Channel)
+
+print(calendar.weekheader(3))
+print()
+
+print(calendar.month(2021, 10))
+# at here you can change month by change at only 1-12 of month
