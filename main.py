c46ad0ce8cf7f477995e83f77b0430875a402370
CalewuLands <92369055+CalewuLands@users.noreply.github.com>
2021-10-13 18:05:21 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ print(calendar.weekheader(3))
 print()
 
 print(calendar.month(2021, 10))
-# at here you can change month by change at only 1-12 of month
+# at here you can change month by change at number 10 only 1-12 of month
